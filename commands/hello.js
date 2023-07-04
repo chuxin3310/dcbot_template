84fd65c45434b184e0d3c787886769ec9b0aae00
Chuxin <113703030+chuxin3310@users.noreply.github.com>
2023-07-04 13:46:34 +0800

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -4,7 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder().setName("hello").setDescription("讓機器人跟你打招呼"),
     async execute(client, interaction) {
         await interaction.reply(
-            `嗨 <@${interaction.user.id}> 很高興認識你，我是 ${client.user.username}`,
+            `嗨ㄐㄐ 很高興認識你，我是 ${client.user.username}`,
         );
     },
 };
