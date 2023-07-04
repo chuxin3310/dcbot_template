e50bfa9c8b537d5a5db4903787c98dd86936d4ff
Chuxin <113703030+chuxin3310@users.noreply.github.com>
2023-07-04 15:46:08 +0800

    54584564

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-    data: new SlashCommandBuilder().setName("ping").setDescription("reply with pong!"),
-    async execute(client, interaction) {
-        await interaction.reply("pong!");
-    },
-};
